df57e2aa640cee6abc962c756a393e4bd1a5f102
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-11-18 14:38:43 -0300

    Stock

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { FilesController } from './files.controller';
 import { CloudinaryProvider } from './cloudinary.provider';
-import { storageCloudinary } from './cloudinary.config';
 
 @Module({
   imports: [
-    MulterModule.register({
-      storage: storageCloudinary,
-    }),
+    MulterModule.register({}),
   ],
   controllers: [FilesController],
   providers: [CloudinaryProvider],
